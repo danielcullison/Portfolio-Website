98690a26e37fdfa5a96053309ec0cf7b307d49a6
DanielCullison <dcullison17@gmail.com>
2024-08-26 23:12:58 -0500

    first commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import "./About.css";
+import theme_pattern from "../../assets/theme_pattern.svg";
+import profile_img from "../../assets/about_profile.svg";
+
+const About = () => {
+  return (
+    <div id="about" className="about">
+      <div className="about-title">
+        <h1>About me</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="about-section">
+        <div className="about-left">
+          <img src={profile_img} alt="" />
+        </div>
+        <div className="about-right">
+          <div className="about-para">
+            <p>
+              I am a skilled software developer with expertise in JavaScript
+              technologies, including React, Redux, Node, and Express. My
+              technical proficiency extends to SQL (PostgreSQL), HTML, and CSS,
+              coupled with strong experience in Git, REST APIs, and relational
+              databases. I have a solid foundation in operating systems, web
+              application security, data security, and data structures and
+              algorithms.
+            </p>
+            <p>
+              Additionally, I am experienced in DevOps practices with a focus on
+              Microsoft Azure. My professional credentials include
+              certifications such as Microsoft Certified Azure Security Engineer
+              Associate, Microsoft Certified Azure Fundamentals, AWS Cloud
+              Practitioner, and CompTIA Security+. These certifications
+              underscore my commitment to cloud security.
+            </p>
+            <p>
+              I have a passion for acquiring new skills and leveraging
+              technology to tackle complex problems. My goal is to contribute
+              innovative solutions that make a meaningful impact in the world of
+              software development.
+            </p>
+          </div>
+          <div className="about-skills">
+            <div className="about-skill">
+              <p>HTML & CSS</p>
+              <hr style={{ width: "50%" }} />
+            </div>
+            <div className="about-skill">
+              <p>React JS</p>
+              <hr style={{ width: "70%" }} />
+            </div>
+            <div className="about-skill">
+              <p>JavaScript</p>
+              <hr style={{ width: "60%" }} />
+            </div>
+            <div className="about-skill">
+              <p>Next JS</p>
+              <hr style={{ width: "50%" }} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="about-achievements">
+        <div className="about-achievement">
+          <h1>10+</h1>
+          <p>YEARS OF EXPERIENCE</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>90+</h1>
+          <p>PROJECTS COMPLETED</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>15+</h1>
+          <p>HAPPY CLIENTS</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Hero.css";
+import profile_img from "../../assets/profile_img.svg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+const Hero = () => {
+  return (
+    <div id="home" className="hero">
+      <img src={profile_img} alt="" />
+      <h1>
+        <span>I'm Daniel Cullison,</span> a Software Developer from New Orleans.
+      </h1>
+      <p>
+        I recently earned a B.S. in Information Systems & Decision Sciences from
+        LSU and also completed a Software Engineering Bootcamp offered by the
+        university.
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect with me
+          </AnchorLink>
+        </div>
+        <div className="hero-resume">My resume</div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
